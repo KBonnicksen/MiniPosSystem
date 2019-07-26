a6eeaf5857390422bf4fee9be748f4ec10646af1
Kevin Stone <ruca6616@gmail.com>
2019-07-26 11:33:28 -0700

    Added comboBox to populate with products, and a button to add a product to the products table. Mainly testing for functionality.

--- MiniPosSystem/Form1.cs
+++ MiniPosSystem/Form1.cs
@@ -16,5 +16,45 @@ namespace MiniPosSystem
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateProductsList();
+        }
+
+        /// <summary>
+        /// Populates comboBox on form with product names from the 
+        /// database
+        /// </summary>
+        private void PopulateProductsList()
+        {
+            List<Products> products = ProductDb.GetProducts();
+
+            cboProducts.DataSource = products;
+
+            cboProducts.DisplayMember = nameof(Products.Name);
+        }
+
+        /// <summary>
+        /// Adds new product to database and repopulates product comboBox
+        /// on form. Mainly for testing purposes as of now (7/26/2019)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            Products newProduct = new Products()
+            {
+                Name = "Margarita Pizza",
+                Price = 10.00m
+            };
+
+            ProductDb.AddProduct(newProduct);
+            string successMessage = $"Added {newProduct.Id}: {newProduct.Name}: {newProduct.Price}";
+            MessageBox.Show(successMessage);
+
+            PopulateProductsList();
+        }
     }
 }
