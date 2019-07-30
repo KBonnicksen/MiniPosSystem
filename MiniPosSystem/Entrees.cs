ad14cb66c99e95f121fb2503c74b2fbd7cb7680a
KBonnicksen <kelseybon@gmail.com>
2019-07-29 20:20:11 -0700

    Added product subclasses

---
+++ MiniPosSystem/Entrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPosSystem
+{
+    public class Entrees : Products
+    {
+        public Entrees()
+        {
+            Category = "Entree";
+        }
+    }
+}
