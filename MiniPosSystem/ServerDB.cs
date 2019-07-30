edc3be2243389467866bcdc1f89c471d999a15ea
KBonnicksen <kelseybon@gmail.com>
2019-07-29 18:16:26 -0700

    Added serverDB class

---
+++ MiniPosSystem/ServerDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPosSystem
+{
+    /// <summary>
+    /// Contains helper methods for manipulating the servers table
+    /// </summary>
+    static class ServerDB
+    {
+        public static List<Servers> GetServers()
+        {
+            using (var context = new OurRestaurantModel())
+            {
+                List<Servers> servers = context.Servers.ToList();
+
+                return servers;
+            }
+        }
+
+        /// <summary>
+        /// Adds new server to database
+        /// </summary>
+        /// <param name="newServer"></param>
+        /// <returns></returns>
+        public static Server AddServer(Server newServer)
+        {
+            using (var context = new OurRestaurantModel())
+            {
+                context.Products.Add(newServer);
+                context.SaveChanges();
+
+                return newServer;
+            }
+        }
+    }
+}
