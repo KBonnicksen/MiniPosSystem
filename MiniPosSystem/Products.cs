cebae59b488bd4f6918349a23b64f013fe341935
KBonnicksen <kelseybon@gmail.com>
2019-07-29 17:22:23 -0700

    Moved Products to external class

---
+++ MiniPosSystem/Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPosSystem
+{
+    /// <summary>
+    /// Represents a single product
+    /// </summary>
+    public class Products
+    {
+        /// <summary>
+        /// The unique Id of a given product 
+        /// (auto-incremented)
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the associated product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The price of the associated product
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
