b21e73ac56e4908cc9e035e080937b9462b7c954
KBonnicksen <kelseybon@gmail.com>
2019-08-01 14:41:06 -0700

    Created basic Payment GUI

---
+++ MiniPosSystem/frmPayment.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MiniPosSystem
+{
+    partial class frmPayment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radCash = new System.Windows.Forms.RadioButton();
+            this.radCard = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cboCardType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radCash
+            // 
+            this.radCash.AutoSize = true;
+            this.radCash.Location = new System.Drawing.Point(35, 50);
+            this.radCash.Name = "radCash";
+            this.radCash.Size = new System.Drawing.Size(61, 21);
+            this.radCash.TabIndex = 0;
+            this.radCash.TabStop = true;
+            this.radCash.Text = "Cash";
+            this.radCash.UseVisualStyleBackColor = true;
+            // 
+            // radCard
+            // 
+            this.radCard.AutoSize = true;
+            this.radCard.Location = new System.Drawing.Point(138, 50);
+            this.radCard.Name = "radCard";
+            this.radCard.Size = new System.Drawing.Size(59, 21);
+            this.radCard.TabIndex = 1;
+            this.radCard.TabStop = true;
+            this.radCard.Text = "Card";
+            this.radCard.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboCardType);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(35, 145);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(486, 241);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Credit Card Information";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(32, 97);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(44, 17);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Total:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(218, 406);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(122, 55);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit Payment";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Card number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name on card:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // cboCardType
+            // 
+            this.cboCardType.FormattingEnabled = true;
+            this.cboCardType.Items.AddRange(new object[] {
+            "Visa",
+            "Mastercard",
+            "American Express",
+            "Discover"});
+            this.cboCardType.Location = new System.Drawing.Point(128, 161);
+            this.cboCardType.Name = "cboCardType";
+            this.cboCardType.Size = new System.Drawing.Size(121, 24);
+            this.cboCardType.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Type:";
+            // 
+            // frmPayment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 473);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radCard);
+            this.Controls.Add(this.radCash);
+            this.Name = "frmPayment";
+            this.Text = "Payment";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radCash;
+        private System.Windows.Forms.RadioButton radCard;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboCardType;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
