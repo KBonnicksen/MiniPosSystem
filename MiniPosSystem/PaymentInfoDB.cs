54f2979919d6c65077eb27eb8c53dfa5f0a92679
KBonnicksen <kelseybon@gmail.com>
2019-08-01 16:08:53 -0700

    created PaymentInfoDB

---
+++ MiniPosSystem/PaymentInfoDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPosSystem
+{
+    class PaymentInfoDB
+    {
+        /// <summary>
+        /// Gets all payments from the database
+        /// </summary>
+        /// <returns></returns>
+        public static List<PaymentInfo> GetPayments()
+        {
+            using (var context = new OurRestaurantModel())
+            {
+                List<PaymentInfo> payments = context.PaymentInfo.ToList();
+
+                return payments;
+            }
+        }
+
+        /// <summary>
+        /// Adds a new payment to the database
+        /// </summary>
+        /// <param name="newPayment"></param>
+        /// <returns></returns>
+        public static PaymentInfo AddPayment(PaymentInfo newPayment)
+        {
+            using (var context = new OurRestaurantModel())
+            {
+                context.PaymentInfo.Add(newPayment);
+                context.SaveChanges();
+
+                return newPayment;
+            }
+        }
+    }
+}
