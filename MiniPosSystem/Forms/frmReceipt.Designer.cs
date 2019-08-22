59976bf98d6a60c0436a5170f4042aa1ea0b7300
KBonnicksen <kelseybon@gmail.com>
2019-08-21 20:43:20 -0700

    Created receipt GUI

---
+++ MiniPosSystem/Forms/frmReceipt.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MiniPosSystem.Forms
+{
+    partial class frmReceipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblCashCard = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCardName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lakewood, Washington";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2513 Wallabe Ave";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Our Restaurant";
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(72, 124);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(50, 17);
+            this.lblServer.TabIndex = 3;
+            this.lblServer.Text = "Server";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(378, 170);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(39, 17);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(378, 132);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(38, 17);
+            this.lblDate.TabIndex = 5;
+            this.lblDate.Text = "Date";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(27, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 180);
+            this.listBox1.TabIndex = 6;
+            // 
+            // lblCashCard
+            // 
+            this.lblCashCard.AutoSize = true;
+            this.lblCashCard.Location = new System.Drawing.Point(72, 479);
+            this.lblCashCard.Name = "lblCashCard";
+            this.lblCashCard.Size = new System.Drawing.Size(74, 17);
+            this.lblCashCard.TabIndex = 7;
+            this.lblCashCard.Text = "Cash/Card";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(72, 420);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(40, 17);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblCardName
+            // 
+            this.lblCardName.AutoSize = true;
+            this.lblCardName.Location = new System.Drawing.Point(72, 450);
+            this.lblCardName.Name = "lblCardName";
+            this.lblCardName.Size = new System.Drawing.Size(97, 17);
+            this.lblCardName.TabIndex = 9;
+            this.lblCardName.Text = "Name on card";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 512);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(273, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Let us know about your dining experience!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 532);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(461, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Email us directly at ourRestaurant@mail.com or call us at 222-123-4567";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(219, 553);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "See you soon!";
+            // 
+            // frmReceipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 629);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCardName);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCashCard);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmReceipt";
+            this.Text = "frmReceipt";
+            this.Load += new System.EventHandler(this.FrmReceipt_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblCashCard;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCardName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ MiniPosSystem/Forms/frmReceipt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPosSystem.Forms
+{
+    public partial class frmReceipt : Form
+    {
+        private readonly Transactions order;
+        public frmReceipt(Transactions tran)
+        {
+            InitializeComponent();
+            order = tran;
+        }
+
+        private void PopulateStoreInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FrmReceipt_Load(object sender, EventArgs e)
+        {
+            PopulateStoreInfo();
+        }
+    }
+}
