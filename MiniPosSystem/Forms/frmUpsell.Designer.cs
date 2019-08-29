7a84bdda80e1fd50ce3db59623e0fa72ea028cc3
Kevin Stone <ruca6616@gmail.com>
2019-08-28 17:34:42 -0700

    Added upsell form

---
+++ MiniPosSystem/Forms/frmUpsell.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MiniPosSystem.Forms
+{
+    partial class frmUpsell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddToOrder = new System.Windows.Forms.Button();
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.lstAddToOrder = new System.Windows.Forms.ListBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnBeverages = new System.Windows.Forms.Button();
+            this.btnEntrees = new System.Windows.Forms.Button();
+            this.btnDesserts = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblAddMe = new System.Windows.Forms.Label();
+            this.lblCurrentOrder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddToOrder
+            // 
+            this.btnAddToOrder.Location = new System.Drawing.Point(21, 286);
+            this.btnAddToOrder.Name = "btnAddToOrder";
+            this.btnAddToOrder.Size = new System.Drawing.Size(120, 43);
+            this.btnAddToOrder.TabIndex = 1;
+            this.btnAddToOrder.Text = "Proceed To Payment";
+            this.btnAddToOrder.UseVisualStyleBackColor = true;
+            this.btnAddToOrder.Click += new System.EventHandler(this.BtnAddToOrder_Click);
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.Location = new System.Drawing.Point(21, 137);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(120, 95);
+            this.lstProducts.TabIndex = 2;
+            this.lstProducts.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LstProducts_MouseDoubleClick);
+            // 
+            // lstAddToOrder
+            // 
+            this.lstAddToOrder.FormattingEnabled = true;
+            this.lstAddToOrder.Location = new System.Drawing.Point(198, 137);
+            this.lstAddToOrder.Name = "lstAddToOrder";
+            this.lstAddToOrder.Size = new System.Drawing.Size(120, 95);
+            this.lstAddToOrder.TabIndex = 3;
+            this.lstAddToOrder.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LstAddToOrder_MouseDoubleClick);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(21, 37);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "label1";
+            // 
+            // btnBeverages
+            // 
+            this.btnBeverages.Location = new System.Drawing.Point(21, 62);
+            this.btnBeverages.Name = "btnBeverages";
+            this.btnBeverages.Size = new System.Drawing.Size(75, 23);
+            this.btnBeverages.TabIndex = 5;
+            this.btnBeverages.Text = "Beverages";
+            this.btnBeverages.UseVisualStyleBackColor = true;
+            this.btnBeverages.Click += new System.EventHandler(this.BtnBeverages_Click);
+            // 
+            // btnEntrees
+            // 
+            this.btnEntrees.Location = new System.Drawing.Point(125, 62);
+            this.btnEntrees.Name = "btnEntrees";
+            this.btnEntrees.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrees.TabIndex = 6;
+            this.btnEntrees.Text = "Entrees";
+            this.btnEntrees.UseVisualStyleBackColor = true;
+            this.btnEntrees.Click += new System.EventHandler(this.BtnEntrees_Click);
+            // 
+            // btnDesserts
+            // 
+            this.btnDesserts.Location = new System.Drawing.Point(229, 62);
+            this.btnDesserts.Name = "btnDesserts";
+            this.btnDesserts.Size = new System.Drawing.Size(75, 23);
+            this.btnDesserts.TabIndex = 7;
+            this.btnDesserts.Text = "Desserts";
+            this.btnDesserts.UseVisualStyleBackColor = true;
+            this.btnDesserts.Click += new System.EventHandler(this.BtnDesserts_Click_1);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(229, 37);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(37, 13);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // lblAddMe
+            // 
+            this.lblAddMe.AutoSize = true;
+            this.lblAddMe.Location = new System.Drawing.Point(21, 118);
+            this.lblAddMe.Name = "lblAddMe";
+            this.lblAddMe.Size = new System.Drawing.Size(47, 13);
+            this.lblAddMe.TabIndex = 9;
+            this.lblAddMe.Text = "Add Me!";
+            // 
+            // lblCurrentOrder
+            // 
+            this.lblCurrentOrder.AutoSize = true;
+            this.lblCurrentOrder.Location = new System.Drawing.Point(198, 117);
+            this.lblCurrentOrder.Name = "lblCurrentOrder";
+            this.lblCurrentOrder.Size = new System.Drawing.Size(70, 13);
+            this.lblCurrentOrder.TabIndex = 10;
+            this.lblCurrentOrder.Text = "Current Order";
+            // 
+            // frmUpsell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 366);
+            this.Controls.Add(this.lblCurrentOrder);
+            this.Controls.Add(this.lblAddMe);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnDesserts);
+            this.Controls.Add(this.btnEntrees);
+            this.Controls.Add(this.btnBeverages);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lstAddToOrder);
+            this.Controls.Add(this.lstProducts);
+            this.Controls.Add(this.btnAddToOrder);
+            this.Name = "frmUpsell";
+            this.Text = "frmUpsell";
+            this.Load += new System.EventHandler(this.FrmUpsell_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAddToOrder;
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.ListBox lstAddToOrder;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnBeverages;
+        private System.Windows.Forms.Button btnEntrees;
+        private System.Windows.Forms.Button btnDesserts;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblAddMe;
+        private System.Windows.Forms.Label lblCurrentOrder;
+    }
+}
---
+++ MiniPosSystem/Forms/frmUpsell.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPosSystem.Forms
+{
+    public partial class frmUpsell : Form
+    {
+        private Transactions existingOrder;
+
+
+        public frmUpsell(Transactions oldOrder)
+        {
+            existingOrder = oldOrder;
+            InitializeComponent();
+        }
+
+        private void FrmUpsell_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Any additional items?";
+            PopulateCurrentOrderList(existingOrder.Products.ToList());
+            List<Products> desserts = ProductDb.GetDesserts();
+            PopulateProductsList(desserts);
+
+        }
+
+        /// <summary>
+        /// Updates the total textbox on the form
+        /// </summary>
+        private void UpdateTotal()
+        {
+            lblTotal.Text = "$" + existingOrder.Price.ToString();
+        }
+
+        /// <summary>
+        /// Populates comboBox on form with product names from the 
+        /// current order
+        /// </summary>
+        private void PopulateCurrentOrderList(List<Products> products)
+        {
+            lstAddToOrder.DataSource = products;
+
+            lstAddToOrder.DisplayMember = nameof(Products.Name);
+
+            UpdateTotal();
+        }
+
+        /// <summary>
+        /// Populates comboBox on form with product names from the 
+        /// database
+        /// </summary>
+        private void PopulateProductsList(List<Products> products)
+        {
+            lstProducts.DataSource = products;
+
+            lstProducts.DisplayMember = nameof(Products.Name);
+        }
+
+        private void BtnEntrees_Click(object sender, EventArgs e)
+        {
+            List<Products> entrees = ProductDb.GetEntrees();
+            PopulateProductsList(entrees);
+        }
+
+    
+
+        private void BtnAddToOrder_Click(object sender, EventArgs e)
+        {
+            ShowPaymentForm();
+        }
+
+        /// <summary>
+        /// Displays form consisting of the current order's information
+        /// </summary>
+        private void ShowPaymentForm()
+        {
+            Form paymentForm = new frmPayment(existingOrder);
+            paymentForm.ShowDialog();
+        }
+
+        private void BtnBeverages_Click(object sender, EventArgs e)
+        {
+            List<Products> beverages = ProductDb.GetBeverages();
+            PopulateProductsList(beverages);
+        }
+
+        private void BtnDesserts_Click_1(object sender, EventArgs e)
+        {
+            List<Products> desserts = ProductDb.GetDesserts();
+            PopulateProductsList(desserts);
+        }
+
+
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            frmPayment payment = new frmPayment(existingOrder);
+            payment.Show();
+        }
+
+        private void LstProducts_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lstProducts.SelectedItem != null)
+            {
+                Products newItem = (Products)lstProducts.SelectedItem;
+                existingOrder.Products.Add(newItem);
+                PopulateCurrentOrderList(existingOrder.Products.ToList());
+            }
+
+        }
+
+        private void LstAddToOrder_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lstAddToOrder.SelectedIndex >= 0)
+            {
+                int current = lstAddToOrder.SelectedIndex;
+                Products selectedProduct = lstAddToOrder.SelectedItem as Products;
+                existingOrder.Products.Remove(selectedProduct);
+                UpdateTotal();
+                PopulateCurrentOrderList(existingOrder.Products.ToList());
+            }
+        }
+    }
+}
